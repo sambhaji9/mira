62d16176873148621e609b8cb370a70d77a20e13
Sambhaji <bbi-m1017@BBI-Ms-MacBook-Pro.local>
2018-06-15 23:03:34 +0530

    Added functiond for showing the apps list, app details. Added some more properties in index json object

--- public/scripts/serverjax.js
+++ public/scripts/serverjax.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
 
+    showAllAppsList();
+
+    var appName = document.getElementById("appName");
+    var appVersion = document.getElementById("appVersion");
+    var appUpdateType = document.getElementById("appUpdateType");
+    var appId = document.getElementById("appId");
+
     var newAppButton = document.getElementById("saveNewApp");
     newAppButton.addEventListener("click", function() {
         // hide the modal
         $("#newAppModal").modal('hide');
 
-        var newApp = new NewApp(document.getElementById("appName").value, document.getElementById("appVersion").value, document.getElementById("appUpdateType").value).addNewApp();
+        if (appId !== "") {
+            var newApp = addNewApp(appName.value, appVersion.value, appUpdateType.value, appId.value);
+        } else
+            new Messages("Please fill all fields").showMessage();
 
     });
 
@@ -14,56 +24,152 @@ $(document).ready(function() {
         $("#appName").trigger("focus");
     });
 
+    $("#appUpdateType").on("change", function() {
+        if (appName.value !== "" && appVersion.value !== "" && appUpdateType.value !== "")
+            generateAppId();
+    });
+
+    $("#appName").on("blur", function() {
+        if (appName.value !== "" && appVersion.value !== "" && appUpdateType.value !== "")
+            generateAppId();
+    });
+
+    $("#appVersion").on("blur", function() {
+        if (appName.value !== "" && appVersion.value !== "" && appUpdateType.value !== "")
+            generateAppId();
+    });
+
+    $("#leftPane").on("click", ".app-text", function() {
+        // get the details of the selected app in left pane
+        selectedAppId = $(this).closest("p").attr("id");
+        var appDetails = getAppDetail(selectedAppId);
+    });
 });
 
-class NewApp {
-    constructor(appName, appVersion, updateType) {
-        this.appName = appName;
-        this.appVersion = appVersion;
-        this.updateType = updateType;
+/**
+ * function generating the app id and displaying in appId input box
+ */
+function generateAppId() {
+    // remove the duplicates from the app name
+    var name = appName.value.split(" ").join("");
+    // make the name as uppercase
+    var tempName = name.substring(0, 3).toUpperCase();
 
-        this.identifier = this.createIdentifier();
+    appId.value = tempName + appVersion.value + ((appUpdateType.value === "Technical") ? "TU" : "SU");
+}
+
+/**
+ * function making an ajax call to add a new version of app
+ */
+function addNewApp(appName, appVersion, appUpdateType, appId) {
+    var xmlHttpRequest = new XMLHttpRequest();
+    if (!xmlHttpRequest) {
+        alert("Cannot instantiate XMLHttpRequest");
+        return false;
     }
-
-    /**
-     * function removing the duplicates and returning the identifier
-     */
-    createIdentifier() {
-        // remove the duplicates from the app name
-        var name = this.appName.split(" ").join("");
-        // make the name as uppercase
-        var tempName = name.substring(0, 3).toUpperCase();
-
-        return tempName + this.appVersion + this.getUpdateType();
-    }
-
-    /**
-     * function returning short form technical update (TU) and server update (SU)
-     * @returns {string} TU or SU based on update type
-     */
-    getUpdateType() {
-        return (this.updateType === "Technical") ? "TU" : "SU";
-    }
-
-    /**
-     * function making an ajax call to add a new version of app
-     */
-    addNewApp() {
-        var xmlHttpRequest = new XMLHttpRequest();
-        if (!xmlHttpRequest) {
-            alert("Cannot instantiate XMLHttpRequest");
-            return false;
-        }
-
-        xmlHttpRequest.onreadystatechange = function() {
-            if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
-                if (xmlHttpRequest.status === 200) {
-                    console.log(xmlHttpRequest.responseText);
-                }
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
+            if (xmlHttpRequest.status === 200) {
+                loadAppsList(JSON.parse(xmlHttpRequest.responseText));
             }
-        };
+        }
+    };
+    xmlHttpRequest.open("GET", "http://127.0.0.1:1689/createNewAppVersion?appName=" + appName + "&appVersion=" + appVersion + "&appUpdateType=" + appUpdateType + "&appId=" + appId, true);
+    xmlHttpRequest.send();
+}
 
-        xmlHttpRequest.open("GET", "http://127.0.0.1:1689/createNewAppVersion?appName=" + this.appName + "&appVersion=" + this.appVersion + "&updateType=" + this.updateType, true);
-        xmlHttpRequest.send();
+/**
+ * function showing all the appsList in leftPane
+ */
+function showAllAppsList() {
+    var xmlHttpRequest = new XMLHttpRequest();
+    if (!xmlHttpRequest) {
+        alert("Cannot instantiate XMLHttpRequest");
+        return false;
     }
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
+            if (xmlHttpRequest.status === 200) {
+                loadAppsList(JSON.parse(xmlHttpRequest.responseText));
+            }
+        }
+    };
+    xmlHttpRequest.open("GET", "http://127.0.0.1:1689/getAllApps", true);
+    xmlHttpRequest.send();
+}
+
+/**
+ * function loading the list in the left pane
+ * @param {array} listArray 
+ */
+function loadAppsList(listArray) {
+    // get the reference to left pane
+    var leftPane = document.getElementById("leftPane");
+    // empty left pane
+    emptyPane(leftPane);
+
+    // get the count of apps
+    var mLength = listArray.length;
+
+    // iterate over the apps list
+    for (var app = 0; app < mLength; app++) {
+        var para = document.createElement("p");
+        para.setAttribute("class", "app-text");
+        // set the id attribute
+        para.setAttribute("id", listArray[app].appId);
+        // set innerHTML
+        para.innerHTML = listArray[app].appName.concat(", ").concat(listArray[app].appVersion);
+        //append appName to left pane
+        leftPane.appendChild(para);
+    }
+}
+
+/**
+ * function removing the child nodes of left pane
+ */
+function emptyPane(pane) {
+    // iterate and remove the child nodes
+    while (pane.firstChild) {
+        pane.removeChild(pane.firstChild);
+    }
+}
+
+function showMessage(message) {
+    // show the warning modal
+    $("#warningModal").modal("show");
+    // display alert text 
+    document.getElementById("alertMessage").innerHTML = message;
+}
+
+/**
+ * function returning the appDetail based on the appId
+ * @param {String} selectedAppId, id of the selected app 
+ */
+function getAppDetail(selectedAppId) {
+    var xmlHttpRequest = new XMLHttpRequest();
+    if (!xmlHttpRequest) {
+        alert("Cannot instantiate XmlHttpRequest");
+        return false;
+    }
+
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState === XMLHttpRequest.DONE) {
+            if (xmlHttpRequest.status === 200) {
+                showAppDetail(xmlHttpRequest.responseText);
+            }
+        }
+    };
+    xmlHttpRequest.open("GET", "http://127.0.0.1:1689/getAppDetail?appId=" + selectedAppId, true);
+    xmlHttpRequest.send();
+}
+
+var selectedApp = {};
+
+/**
+ * function showing the app details
+ * @param {object} appObj 
+ */
+function showAppDetail(appObj) {
+    selectedApp = appObj;
+    console.log(selectedApp);
 }
--- server.js
+++ server.js
@@ -12,22 +12,54 @@ app.get("/", function(request, response) {
 });
 
 app.get("/createNewAppVersion", function(request, response) {
-    console.log(request.query.appVersion);
-    fileSys.readFile("./public/files/index.json", "utf-8", function(err, data) {
-        // parse the array
-        var projectArray = JSON.parse(data);
-        projectArray.push({
-            "appName": request.query.appName,
-            "appVersion": request.query.appVersion,
-            "updateType": request.query.updateType
-        });
-        console.log(projectArray);
-        fileSys.writeFile("./public/files/index.json", JSON.stringify(projectArray, null, 4), function(err) {
-            if (err) console.log("Unable to write index.json file");
-        });
+    var data = fileSys.readFileSync("./public/files/index.json", "utf-8");
+    // parse the array
+    var projectArray = JSON.parse(data);
+    projectArray.push({
+        "appName": request.query.appName,
+        "appVersion": request.query.appVersion,
+        "appUpdateType": request.query.appUpdateType,
+        "appId": request.query.appId,
+        "date": new Date().toLocaleString()
     });
+    // rewrite the index.json file
+    fileSys.writeFileSync("./public/files/index.json", JSON.stringify(projectArray, null, 4));
+
+    // write the details file
+    fileSys.writeFile("./public/files/" + request.query.appId + ".json", "[]", function(err) {
+        if (err)
+            console.error(err);
+    });
+
+    response.send(getIndexJson());
+});
+
+app.get("/getAllApps", function(request, response) {
+    response.send(getIndexJson());
+});
+
+app.get("/getAppDetail", function(request, response) {
+    // get the apps and parse the list
+    var allApps = JSON.parse(getIndexJson());
+
+    var appId = request.query.appId;
+
+    // filter and return the app object, matching the appId
+    var appDetail = allApps.filter(function(app) {
+        if (request.query.appId === app.appId)
+            return app;
+    });
+    response.send(appDetail);
 });
 
 app.listen(1689, function() {
     console.log("Server listening on http://localhost:1689/");
 });
+
+/**
+ * function reading and returning the index.json file
+ * return {array}
+ */
+function getIndexJson() {
+    return fileSys.readFileSync("./public/files/index.json", "utf-8");
+}
